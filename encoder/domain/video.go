313c525c3637f7992bb9d22650b4f5fca67fddd6
nobruin <nobruin@gmail.com>
2024-01-02 17:05:55 -0300

    feature: add gorm domain structs

--- encoder/domain/video.go
+++ encoder/domain/video.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Video struct {
-	ID         string    `valid:"uuid"`
-	ResourceId string    `valid:"notnull"`
-	FilePath   string    `valid:"notnull"`
-	CreatedAT  time.Time `valid:"-"`
+	ID         string    `json:"encoded_video_folder" valid:"uuid" gorm:"type:uuid;primary_key"`
+	ResourceId string    `json:"resource_id" valid:"notnull" gorm:"type:varchar(255)"`
+	FilePath   string    `json:"file_path" valid:"notnull" gorm:"type:varchar(255)"`
+	CreatedAT  time.Time `json:"created_at" valid:"-"`
+	Jobs       []*Job    `json:"-" valid:"-" gorm:"ForeignKey:VideoID"`
 }
 
 func init() {
